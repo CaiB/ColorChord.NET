ed34c15b7da7d81655399d34725851a06e0bc294
Cai Biesinger <cai.biesinger@outlook.com>
2020-12-27 23:22:15 -0800

    Rewrote Cells visualizer with documentation and better names. Should remain functionally identical to base ColorChord.

--- ColorChord.NET/Visualizers/Cells.cs
+++ ColorChord.NET/Visualizers/Cells.cs
@@ -9,23 +9,47 @@ namespace ColorChord.NET.Visualizers
 {
     public class Cells : IVisualizer, IDiscrete1D
     {
+        /// <summary> The number of discrete points to be output. Usually equals the number of LEDs on a physical system. </summary>
         public int LEDCount { get; set; }
+
+        /// <summary> A unique name for this visualizer instance, used for referring to it from other components. </summary>
         public string Name { get; set; }
+
+        /// <summary> Whether or not this visualizer is currently operating. </summary>
         public bool Enabled { get; set; }
 
         public int FramePeriod = 1000 / 90;
 
+        /// <summary> All outputs that need to be notified when new data is available. </summary>
         private readonly List<IOutput> Outputs = new List<IOutput>();
-        public uint[] OutputData;
+
+        /// <summary> Stores the latest data to be given to <see cref="IOutput"/> modules when requested. </summary>
+        private uint[] OutputData;
+
+        /// <summary> Whether to continue processing, or stop threads and finish up in preparation for closing the application. </summary>
         private bool KeepGoing = true;
+
+        /// <summary> THe thread on which input note data is processed by this visualizer. </summary>
         private Thread ProcessThread;
 
-        public float LEDFloor { get; set; }
+        /// <summary> How strongly notes should be amplified before processing. </summary>
         public float LightSiding { get; set; }
+
+        /// <summary> How much saturation (LED brightness) should be amplified before sending the output data. </summary>
         public float SaturationAmplifier { get; set; }
+
+        /// <summary> A multiplier applied to input note strengths to determine how many LEDs should become that colour. </summary>
+        /// <remarks> This should be proportionally increased/decreased if the number of LEDs changes to keep the same overall percentage of LEDs on the same. </remarks>
         public float QtyAmp { get; set; }
+
+        /// <summary> Whether to use smoothed input data, reducing flicker, or only the latest data. </summary>
         public bool SteadyBright { get; set; }
-        public bool TimeBased { get; set; } // Useful for pies, turn off for linear systems.
+
+        /// <summary> Whether LEDs turning on/off should be based on how long since their last state change (true), or based solely on position (false). </summary>
+        /// <remarks> Useful for pies, turn off for linear systems. </remarks>
+        public bool TimeBased { get; set; }
+
+        // TODO: Understand & document this feature once it works in base ColorChord.
         public bool Snakey { get; set; } // Advance head for where to get LEDs around.
 
         public Cells(string name) { this.Name = name; }
@@ -38,7 +62,6 @@ namespace ColorChord.NET.Visualizers
             this.LEDCount = ConfigTools.CheckInt(options, "LEDCount", 1, 100000, 50, true);
             this.FramePeriod = 1000 / ConfigTools.CheckInt(options, "FrameRate", 0, 1000, 60, true);
 
-            this.LEDFloor = ConfigTools.CheckFloat(options, "LEDFloor", 0, 1, 0.1F, true);
             this.LightSiding = ConfigTools.CheckFloat(options, "LightSiding", 0, 100, 1.9F, true);
             this.SaturationAmplifier = ConfigTools.CheckFloat(options, "SaturationAmplifier", 0, 100, 2F, true);
             this.QtyAmp = ConfigTools.CheckFloat(options, "QtyAmp", 0, 100, 20, true);
@@ -49,12 +72,8 @@ namespace ColorChord.NET.Visualizers
             ConfigTools.WarnAboutRemainder(options, typeof(IVisualizer));
 
             this.OutputData = new uint[this.LEDCount];
-
-            this.led_note_attached = new int[this.LEDCount];
-            this.time_of_change = new double[this.LEDCount];
-            this.last_led_pos = new float[this.LEDCount];
-            this.last_led_pos_filter = new float[this.LEDCount];
-            this.last_led_amp = new float[this.LEDCount];
+            this.LEDBinMapping = new int[this.LEDCount];
+            this.LastChangeTime = new double[this.LEDCount];
         }
 
         public void Start()
@@ -89,162 +108,149 @@ namespace ColorChord.NET.Visualizers
         public int GetCountDiscrete() => this.LEDCount;
         public uint[] GetDataDiscrete() => this.OutputData;
 
-        #region ColorChord Magic
-        private int[] led_note_attached;
-        private double[] time_of_change;
-
-        private float[] last_led_pos;
-        private float[] last_led_pos_filter;
-        private float[] last_led_amp;
-        private int snakeyplace = 0;
+        // These are used between frames of output data. Only used in Update().
+        private int[] LEDBinMapping; // Contains each LED's note bin ID.
+        private double[] LastChangeTime; // When each LED last changed state.
+        private int LastChangeIndex = 0; // Where we last made changes, only used in snakey mode.
 
         private void Update()
         {
-            //Step 1: Calculate the quantity of all the LEDs we'll want.
-            int totbins = BaseNoteFinder.NoteCount;//nf->dists;
-            int i, j;
-            float[] binvals = new float[totbins];
-            float[] binvalsQ = new float[totbins];
-            float[] binpos = new float[totbins];
-            float totalbinval = 0;
+            const int BinCount = BaseNoteFinder.NoteCount;
 
-            float[] qtyHave = new float[totbins];
-            float[] qtyWant = new float[totbins];
-            float totQtyWant = 0;
-
-            for (i = 0; i < this.LEDCount; i++)
+            // Determine how many LEDs of each colour there currently are on from the previous cycle.
+            float[] LEDsPerBin = new float[BinCount];
+            for (int i = 0; i < this.LEDCount; i++)
             {
-                int l = this.led_note_attached[i];
-                if (l >= 0) { qtyHave[l]++; }
+                int Bin = this.LEDBinMapping[i];
+                if (Bin >= 0) { LEDsPerBin[Bin]++; }
             }
 
-
-            for (i = 0; i < totbins; i++)
+            // Determine the colour and value for each note present in new input data, then figure out how many LEDs should be on in that colour.
+            float[] BinColours = new float[BinCount]; // Range of 0-1, colour of each bin
+            float[] BinValuesSlow = new float[BinCount]; // Note amplitude, slightly smoothed to reduce flicker.
+            float[] BinValues = new float[BinCount]; // Note amplitude, not smoothed.
+            float BinValuesSum = 0;
+            float[] LEDsDesired = new float[BinCount];
+            float TotalDesiredCount = 0; // How many LEDs we'd want on, taking into account the quantity for each colour
+            for (int i = 0; i < BinCount; i++)
             {
-                binpos[i] = BaseNoteFinder.Notes[i].Position / BaseNoteFinder.OctaveBinCount;
-                binvals[i] = (float)Math.Pow(BaseNoteFinder.Notes[i].AmplitudeFiltered, this.LightSiding); //Slow
-                binvalsQ[i] = (float)Math.Pow(BaseNoteFinder.Notes[i].Amplitude, this.LightSiding); //Fast
-                totalbinval += binvals[i];
+                BinColours[i] = BaseNoteFinder.Notes[i].Position / BaseNoteFinder.OctaveBinCount;
+                BinValuesSlow[i] = (float)Math.Pow(BaseNoteFinder.Notes[i].AmplitudeFiltered, this.LightSiding);
+                BinValues[i] = (float)Math.Pow(BaseNoteFinder.Notes[i].Amplitude, this.LightSiding);
+                BinValuesSum += BinValuesSlow[i];
 
-                float want = binvals[i] * this.QtyAmp;
-                totQtyWant += want;
-                qtyWant[i] = want;
+                float DesiredCount = BinValuesSlow[i] * this.QtyAmp; // How many LEDs we'd like on for this colour
+                TotalDesiredCount += DesiredCount;
+                LEDsDesired[i] = DesiredCount;
             }
 
-            if (totQtyWant > this.LEDCount)
+            // We want more LEDs on than we have. Scale all counts down to fit our output size.
+            if (TotalDesiredCount > this.LEDCount)
             {
-                float overage = this.LEDCount / totQtyWant;
-                for (i = 0; i < totbins; i++) { qtyWant[i] *= overage; }
+                float ScaleFactor = this.LEDCount / TotalDesiredCount;
+                for (int i = 0; i < BinCount; i++) { LEDsDesired[i] *= ScaleFactor; }
             }
 
-            float[] qtyDiff = new float[totbins];
-            for (i = 0; i < totbins; i++)
-            {
-                qtyDiff[i] = qtyWant[i] - qtyHave[i];
-            }
+            // Determine how many LEDs we'd like to turn on (+) or off (-) based on how many we want on, and how many are on.
+            float[] DesiredChange = new float[BinCount];
+            for (int i = 0; i < BinCount; i++) { DesiredChange[i] = LEDsDesired[i] - LEDsPerBin[i]; }
 
-            //Step 1: Relinquish LEDs
-            for (i = 0; i < totbins; i++)
+            // Find LEDs to turn off.
+            for (int BinInd = 0; BinInd < BinCount; BinInd++)
             {
-                while (qtyDiff[i] < -0.5)
+                while (DesiredChange[BinInd] < -0.5) // We want to reduce LED count of this colour.
                 {
-                    double maxtime = -1.0;
-                    int maxindex = -1;
+                    double MostRecentChange = -1.0; // When the youngest LED of this colour turned on.
+                    int MostRecentChangeIndex = -1; // The index of the above LED.
 
-                    //Find the LEDs that have been on least.
-                    for (j = 0; j < this.LEDCount; j++)
+                    // Finds the LEDs that have been on for the shortest amount of time.
+                    for (int LEDInd = 0; LEDInd < this.LEDCount; LEDInd++)
                     {
-                        if (this.led_note_attached[j] != i) continue;
-                        if (!this.TimeBased) { maxindex = j; break; }
-                        if (this.time_of_change[j] > maxtime)
+                        if (this.LEDBinMapping[LEDInd] != BinInd) { continue; } // This LED is a different colour, or already off.
+                        if (!this.TimeBased) { MostRecentChangeIndex = LEDInd; break; }
+                        if (this.LastChangeTime[LEDInd] > MostRecentChange)
                         {
-                            maxtime = this.time_of_change[j];
-                            maxindex = j;
+                            MostRecentChange = this.LastChangeTime[LEDInd];
+                            MostRecentChangeIndex = LEDInd;
                         }
                     }
-                    if (maxindex >= 0)
+                    // Turn off the LED that has been on the shortest.
+                    if (MostRecentChangeIndex >= 0)
                     {
-                        this.led_note_attached[maxindex] = -1;
-                        this.time_of_change[maxindex] = Now();
+                        this.LEDBinMapping[MostRecentChangeIndex] = -1;
+                        this.LastChangeTime[MostRecentChangeIndex] = Now();
                     }
-                    qtyDiff[i]++;
+                    DesiredChange[BinInd]++;
                 }
             }
 
-            //Throw LEDs back in.
-            for (i = 0; i < totbins; i++)
+            // Find LEDs to turn on. Pattern depends on whether snakey is on.
+            for (int BinInd = 0; BinInd < BinCount; BinInd++)
             {
-                while (qtyDiff[i] > 0.5)
+                while (DesiredChange[BinInd] > 0.5) // We want to increase LED count of this colour.
                 {
-                    double seltime = 1e20;
-                    int selindex = -1;
+                    double LeastRecentChange = 1E20;
+                    int LeastRecentChangeIndex = -1;
 
-
-                    //Find the LEDs that haven't been on in a long time.
-                    for (j = 0; j < this.LEDCount; j++)
+                    // Find an LED suitable to turn on.
+                    for (int LEDInd = 0; LEDInd < this.LEDCount; LEDInd++)
                     {
-                        if (this.led_note_attached[j] != -1) continue;
-                        if (!this.TimeBased) { selindex = j; break; }
+                        if (this.LEDBinMapping[LEDInd] != -1) { continue; } // This LED is already on.
+                        if (!this.TimeBased) { LeastRecentChangeIndex = LEDInd; break; }
 
-                        if (this.Snakey)
+                        if (this.Snakey) // TODO: Finish rewriting this section. Base ColorChord's functionality here does not currently work, so waiting on that to be fixed.
                         {
-                            float bias = 0;
-                            float timeimp = 1;
+                            //float timeimp = 1;
 
-                            bias = (j - this.snakeyplace + this.LEDCount) % this.LEDCount;
+                            float DistanceFromLast = (LEDInd - this.LastChangeIndex + this.LEDCount) % this.LEDCount;
 
-                            if (bias > this.LEDCount / 2) bias = this.LEDCount - bias + 1;
-                            timeimp = 0;
+                            if (DistanceFromLast > this.LEDCount / 2) { DistanceFromLast = this.LEDCount - DistanceFromLast + 1; }
+                            //timeimp = 0;
 
-                            float score = (float)(this.time_of_change[j] * timeimp + bias);
-                            if (score < seltime)
+                            float score = (float)(/*this.LastChangeTime[LEDInd] * timeimp + */DistanceFromLast);
+                            if (score < LeastRecentChange)
                             {
-                                seltime = score;
-                                selindex = j;
+                                LeastRecentChange = score;
+                                LeastRecentChangeIndex = LEDInd;
                             }
                         }
-                        else
+                        else // In non-snakey mode, find the LED that has been off the longest.
                         {
-                            if (this.time_of_change[j] < seltime)
+                            if (this.LastChangeTime[LEDInd] < LeastRecentChange)
                             {
-                                seltime = this.time_of_change[j];
-                                selindex = j;
+                                LeastRecentChange = this.LastChangeTime[LEDInd];
+                                LeastRecentChangeIndex = LEDInd;
                             }
                         }
                     }
-                    if (selindex >= 0)
+                    if (LeastRecentChangeIndex >= 0) // We found a suitable LED, turn it on.
                     {
-                        this.led_note_attached[selindex] = i;
-                        this.time_of_change[selindex] = Now();
-                        this.snakeyplace = selindex;
+                        this.LEDBinMapping[LeastRecentChangeIndex] = BinInd;
+                        this.LastChangeTime[LeastRecentChangeIndex] = Now();
+                        this.LastChangeIndex = LeastRecentChangeIndex;
                     }
-                    qtyDiff[i]--;
+                    DesiredChange[BinInd]--;
                 }
             }
 
-            //Advance the LEDs to this position when outputting the values.
-            for (i = 0; i < this.LEDCount; i++)
+            // Update the output array with the new colours.
+            for (int LEDInd = 0; LEDInd < this.LEDCount; LEDInd++)
             {
-                int ia = this.led_note_attached[i];
-                if (ia == -1)
-                {
-                    this.OutputData[i] = 0;
-                    continue;
-                }
-                float sat = binvals[ia] * this.SaturationAmplifier;
-                float satQ = binvalsQ[ia] * this.SaturationAmplifier;
-                if (satQ > 1) satQ = 1;
-                float sendsat = (this.SteadyBright ? sat : satQ);
-                if (sendsat > 1) sendsat = 1;
-                uint r = VisualizerTools.CCtoHEX(binpos[ia], 1.0F, sendsat);
+                int BinID = this.LEDBinMapping[LEDInd]; // Which bin this LED is using.
 
-                this.OutputData[i] = r;
+                if (BinID == -1) { this.OutputData[LEDInd] = 0; } // This LED is off.
+                else
+                {
+                    float SaturationSlow = BinValuesSlow[BinID] * this.SaturationAmplifier;
+                    float Saturation = BinValues[BinID] * this.SaturationAmplifier;
+                    float SaturationUsed = (this.SteadyBright ? SaturationSlow : Saturation);
+                    if (SaturationUsed > 1) { SaturationUsed = 1; }
+                    this.OutputData[LEDInd] = VisualizerTools.CCtoHEX(BinColours[BinID], 1.0F, SaturationUsed);
+                }
             }
         }
 
         private double Now() => DateTime.UtcNow.Ticks / 10000000D;
-
-        #endregion
 
     }
 }
