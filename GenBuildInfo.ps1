c82483703d7faf170e1f3e782ffb93f306c76ee2
CaiB <Cai.Biesinger@outlook.com>
2023-04-22 14:23:47 -0600

    Take 2 of fixing the build script

--- GenBuildInfo.ps1
+++ GenBuildInfo.ps1
@@ -2,12 +2,16 @@ param($ConfigHashFile, $ConfigFile);
 
 Write-Host "Putting default config hash into file '$ConfigHashFile'.";
 
-[string] $Hash = (Get-FileHash $ConfigFile -Algorithm 'MD5').Hash;
-
-$HashObj = New-Object -TypeName 'System.Security.Cryptography.MD5CryptoServiceProvider'
-$FileStream = [System.IO.File]::Open((Resolve-Path $ConfigFile), [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite)
-try { [System.BitConverter]::ToString($HashObj.ComputeHash($FileStream)) }
-finally { $FileStream.Dispose() }
+$HashObj = New-Object -TypeName 'System.Security.Cryptography.MD5CryptoServiceProvider';
+$FileStream = [System.IO.File]::Open((Resolve-Path $ConfigFile), [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite);
+try
+{
+    [byte[]] $HashBytes = $HashObj.ComputeHash($FileStream);
+    $HexBuilder = [System.Text.StringBuilder]::new($HashBytes.Length * 2);
+    foreach($b in $HashBytes) { $HexBuilder.AppendFormat("{0:X2}", $b); }
+    [string] $Hash = $HexBuilder.ToString();
+}
+finally { $FileStream.Dispose(); }
 
 $Class = 
 @"
