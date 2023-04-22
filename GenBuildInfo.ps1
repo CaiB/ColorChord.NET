72a574886ba52ad0bd7b6743048836614f372d53
CaiB <Cai.Biesinger@outlook.com>
2023-04-22 14:07:33 -0600

    AppVeyor appears to have an outdated version of PowerShell, working around that

--- GenBuildInfo.ps1
+++ GenBuildInfo.ps1
@@ -4,6 +4,11 @@ Write-Host "Putting default config hash into file '$ConfigHashFile'.";
 
 [string] $Hash = (Get-FileHash $ConfigFile -Algorithm 'MD5').Hash;
 
+$HashObj = New-Object -TypeName 'System.Security.Cryptography.MD5CryptoServiceProvider'
+$FileStream = [System.IO.File]::Open((Resolve-Path $ConfigFile), [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite)
+try { [System.BitConverter]::ToString($HashObj.ComputeHash($FileStream)) }
+finally { $FileStream.Dispose() }
+
 $Class = 
 @"
 namespace ColorChord.NET.Config;
