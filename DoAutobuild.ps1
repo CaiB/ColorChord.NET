e568cac9e43cf7d8587046532e210d0c5fa5f324
CaiB <Cai.Biesinger@outlook.com>
2024-05-13 22:54:04 +0900

    Trying to fix autobuild

--- DoAutobuild.ps1
+++ DoAutobuild.ps1
@@ -1,13 +1,13 @@
 & nuget restore
 
 & dotnet publish "$env:APPVEYOR_BUILD_FOLDER\ColorChord.NET\ColorChord.NET.csproj" --output "$env:APPVEYOR_BUILD_FOLDER\ColorChord.NET\PublishResult" --configuration Release --verbosity minimal
-if ($LastExitCode -NE 0) { Exit -1; }
+if ($LastExitCode -NE 0) { [Environment]::Exit(-1); }
 
 $ExtensionDirs = Get-ChildItem "$env:APPVEYOR_BUILD_FOLDER\Extensions\" -Directory
 $ExtensionDirs | ForEach-Object `
 {
     & dotnet publish "$env:APPVEYOR_BUILD_FOLDER\Extensions\$($_.Name)\$($_.Name).csproj" --output "$env:APPVEYOR_BUILD_FOLDER\Extensions\PublishResult" -p:OutputPath="$env:APPVEYOR_BUILD_FOLDER\Extensions\PublishResult" --configuration Release --verbosity minimal
-    if ($LastExitCode -NE 0) { Exit -1; }
+    if ($LastExitCode -NE 0) { [Environment]::Exit(-2); }
 }
 
 # Remove-Item "$env:APPVEYOR_BUILD_FOLDER\Extensions\PublishResult\ColorChord.NET-API.*"
